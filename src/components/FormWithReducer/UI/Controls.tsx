b3b75ee3bb21b535be8156959659ca6ecbcd3cb9
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-18 18:11:26 -0800

    WIP. form. add Select component

--- src/components/FormWithReducer/UI/Controls.tsx
+++ src/components/FormWithReducer/UI/Controls.tsx
@@ -1,5 +1,6 @@
-import { HTMLAttributes, InputHTMLAttributes } from "react";
+import { HTMLAttributes, InputHTMLAttributes, SelectHTMLAttributes } from "react";
 import { classNames } from "@/utils/classnames";
+import "./controls.css";
 
 function ErrorMessage({ error, className }: { error?: string; } & HTMLAttributes<HTMLDivElement>) {
     return (
@@ -9,7 +10,6 @@ function ErrorMessage({ error, className }: { error?: string; } & HTMLAttributes
         >
             {error}
         </div>
-
     );
 }
 
@@ -22,8 +22,9 @@ export function Input({ label, error, className, ...rest }: { label: string; err
 
             <input
                 className={classNames(
-                    "px-4 py-1.5 text-indigo-900 bg-indigo-200 rounded",
+                    "px-3 py-1.5 text-indigo-900 bg-indigo-200 rounded",
                     "outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-100/70 focus:ring-offset-indigo-500/70",
+                    "placeholder:text-indigo-300",
                     className
                 )}
                 {...rest}
@@ -46,13 +47,37 @@ export function Checkbox({ label, error, className, ...rest }: { label: string; 
                     )}
                     {...rest}
                 />
+
                 <div className="mb-1 text-indigo-200">
                     {label}
                 </div>
-
             </div>
 
             <ErrorMessage error={error} />
         </label>
     );
+}
+
+export function Select({ label, error, children, ...rest }: { label: string; error?: string; } & SelectHTMLAttributes<HTMLSelectElement>) {
+    return (<>
+        <label className="grid select-none">
+            <div className="mb-1 text-indigo-200">
+                {label}
+            </div>
+
+            <select
+                {...rest}
+                className={classNames(
+                    "px-3 py-1.5 text-indigo-900 bg-indigo-200 rounded",
+                    "outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-100/70 focus:ring-offset-indigo-500/70",
+                    "tm-input-select",
+                    !!error && "ring-red-400 ring-2",
+                )}
+            >
+                {children}
+            </select>
+
+            <ErrorMessage error={error} />
+        </label>
+    </>);
 }
