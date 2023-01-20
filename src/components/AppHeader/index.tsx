2096130e81cf071f2a1402adee7ecb6fca2e38b9
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-19 19:06:21 -0800

    WIP. app. add AppHeader.Button component

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 import { useSetAtom } from 'jotai';
 import { currentScreenAtom } from '@/store';
 
-export function AppHeader() {
+function Button({ form, children, ...rest }: { form: number; } & HTMLAttributes<HTMLButtonElement>) {
     const setScreen = useSetAtom(currentScreenAtom);
+    return (
+        <button onClick={() => setScreen(form)} {...rest}>
+            {children}
+        </button>
+    );
+}
+
+export function AppHeader() {
     return (
         <ul className="px-4 py-2 text-xl font-semibold text-indigo-800 flex justify-center space-x-4">
             <li>
-                <button onClick={() => setScreen(0)}>Non-Modal Dialog</button>
+                <Button form={0}>Non-Modal Dialog</Button>
             </li>
             <li>
-                <button onClick={() => setScreen(1)}>Modal Dialog</button>
+                <Button form={1}>Modal Dialog</Button>
             </li>
         </ul>
     );
