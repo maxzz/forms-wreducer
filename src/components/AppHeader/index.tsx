dc4edb51207086302f17e7d8f444ee90d959e581
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-19 19:11:40 -0800

    WIP. AppHeader. update Buttons look

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,11 +1,17 @@
 import React, { HTMLAttributes } from 'react';
 import { useSetAtom } from 'jotai';
 import { currentScreenAtom } from '@/store';
+import { classNames } from '@/utils/classnames';
 
 function Button({ form, children, ...rest }: { form: number; } & HTMLAttributes<HTMLButtonElement>) {
     const setScreen = useSetAtom(currentScreenAtom);
     return (
-        <button onClick={() => setScreen(form)} {...rest}>
+        <button
+            className={classNames(
+                "px-4 py-2 text-xl font-semibold text-indigo-800 bg-indigo-200 hover:bg-indigo-100 active:scale-[.97]",
+            )}
+            onClick={() => setScreen(form)} {...rest}
+        >
             {children}
         </button>
     );
@@ -13,13 +19,9 @@ function Button({ form, children, ...rest }: { form: number; } & HTMLAttributes<
 
 export function AppHeader() {
     return (
-        <ul className="px-4 py-2 text-xl font-semibold text-indigo-800 flex justify-center space-x-4">
-            <li>
-                <Button form={0}>Non-Modal Dialog</Button>
-            </li>
-            <li>
-                <Button form={1}>Modal Dialog</Button>
-            </li>
-        </ul>
+        <div className="flex justify-center space-x-2">
+            <Button form={0}>Non-Modal Dialog</Button>
+            <Button form={1}>Modal Dialog</Button>
+        </div>
     );
 }
