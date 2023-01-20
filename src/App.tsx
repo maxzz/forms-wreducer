9e39ae1475e90b892dff4017502de4469244215f
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-19 18:45:08 -0800

    WIP. add dialogs basic switch

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,29 @@ import { BWSwitch } from '@/components/BWSwitch';
 
 function App() {
     return (
-        <div className="h-screen relative debug-screens" style={{ backgroundImage: 'radial-gradient(circle at right bottom, #85b2ff, #9fc7ff)' }}>
-            <BWSwitch />
+        <div
+            className="h-screen relative debug-screens flex flex-col"
+            style={{ backgroundImage: 'radial-gradient(circle at right bottom, #85b2ff, #9fc7ff)' }}
+        >
+            <div>
+                <ul className="px-4 py-2 text-xl font-semibold text-indigo-800 flex justify-center space-x-4">
+                    <li>
+                        <button>Non-Modal Dialog</button>
+                    </li>
+                    <li>
+                        <button>Modal Dialog</button>
+                    </li>
+                </ul>
+
+                <BWSwitch />
+            </div>
+            
             <FormWithReducer />
         </div>
     );
 }
 
 export default App;
+
+//TODO: yup validation
+//TODO: <dialog>
