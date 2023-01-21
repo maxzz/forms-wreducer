6fcfdd0c477f7a649c58c8c19296b4f6c59f56c2
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-20 20:03:30 -0800

    WIP. utils. update index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './classnames';
 export * from './useClientEffect'; //https://github.com/mui/material-ui/blob/master/packages/mui-utils/src/useEnhancedEffect.js
+export * from './useEventCallback'; //https://github.com/mui/material-ui/blob/master/packages/mui-utils/src/useEventCallback.ts
