448d7a6f9c79665102ff36828348c05a1caf83f0
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-20 17:58:07 -0800

    WIP. utils. add debounce()

---
+++ src/utils/debounce.ts
@@ -0,0 +1,31 @@
+/**
+ * Corresponds to 10 frames at 60 Hz.
+ * A few bytes payload overhead when lodash/debounce is ~3 kB and debounce ~300 B.
+ * 
+ * https://github.com/mui/material-ui/blob/master/packages/mui-utils/src/debounce.js
+ * 
+ * @param func 
+ * @param wait 
+ * @returns 
+ */
+export interface Cancelable {
+    clear(): void;
+}
+
+export function debounce<T extends (this: any, ...args: any[]) => any, This>(func: T, wait: number = 166): T & Cancelable {
+    let timeout: ReturnType<typeof setTimeout>;
+    
+    function debounced(this: any, ...args: any[]) {
+        const later = () => {
+            func.apply(this, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    }
+
+    debounced.clear = () => {
+        clearTimeout(timeout);
+    };
+
+    return debounced as T & Cancelable;
+}
